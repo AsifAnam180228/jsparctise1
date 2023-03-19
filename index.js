60f423f3da0a6a84b8b2aefbedfc3098b9a4f57c
AsifAnam180228 <anam1828@cseku.ac.bd>
2023-03-19 14:40:29 +0600

    19-03-2023

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-let firstCard = 10
-let secondCard = 10
+
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById('message-el')
-let sumEl = document.getElementById('sum-el')
+let sumEl = document.querySelector('#sum-el')
 let cardEl = document.querySelector('#cards-el')
-console.log(cardEl)
+let logEl = document.getElementById('log')
 
-cardEl.textContent = "Cards: "+ firstCard + " "+ secondCard
+let player = {
+    Name:  "Shaon",
+    Chips: 145
+}
 
-let sum = firstCard + secondCard
-sumEl.textContent = "Sum: "+ sum
+let playerEl = document.getElementById('player-el')
+playerEl.textContent = player.Name +": $" +player.Chips
+
+console.log(cards)
+
+function getRandomCard()
+{
+    let randomNum = 1+ Math.floor(Math.random()*13)
+    if(randomNum === 1){
+        return 11
+    }
+    else if(randomNum>10){
+        return 10
+    }
+    else
+        return randomNum
+}
 
 function startGame()
 {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard)
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+function renderGame()
+{
+    cardEl.textContent = "Cards: "
+
+    for(let i = 0; i<cards.length; i++){
+        cardEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: "+ sum
+
     if(sum <= 20){
         message = "Do you want to draw a new card?"
         
@@ -34,6 +73,57 @@ function startGame()
 
 function newCard()
 {
-    console.log("Drawing a new card")
+    if(isAlive && !hasBlackjack){
+        console.log("Drawing a new card")
+        let thirdCard = getRandomCard()
+        cards.push(thirdCard)
+        sum += thirdCard
+        console.log(cards)
+        renderGame()
+    }
+    
+
 }
+// let person = {
+//     name: "Asif Anam",
+//     age: 24,
+//     country: "Bangladesh"
+// }
+// function logData()
+// {
+//     let str = person.name + " is " + person.age + " years old and lives in "+ person.country
+
+//     return str
+// }
+
+// let age = 35
+
+// if(age<6){
+//     console.log("Free")
+// }
+// else if(age>6 && age<17){
+//     console.log("Child discount")
+// }
+// else if(age>17 && age<26){
+//     console.log("student discount")
+// }
+// else if(age>26 && age<66){
+//     console.log("full price")
+// }
+// else
+//     console.log("Senior citizen discount")
+
+
+// let largeCounties = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+// largeCounties.shift()
+// largeCounties.unshift("China")
+// largeCounties.pop()
+// largeCounties.push("Pak")
+
+// let hands = ["rock", "paper", "secissor"]
+// let randid = Math.random()
+
+// let randIndex = Math.floor(randid*3)
+
+// console.log(hands[randIndex])
 
